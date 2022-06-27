4141bfac051105061407efb42849035941415dc5
Ridealist <cjsn92@gmail.com>
2022-06-28 07:35:12 +0900

    Implement vilageFcst & Upgrade UltraSrtFcst funcs.

--- kakaochat/views.py
+++ kakaochat/views.py
@@ -81,7 +81,7 @@ def weekday(request):
     return JsonResponse(res)
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 def UltraSrtNcst(request):
     
     weather_url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?"
@@ -93,10 +93,15 @@ def UltraSrtNcst(request):
 
     hour = datetime.now().hour
     # TODO by minutes making accurate base_time hour settings needed
-    # datetime.now().minute
+    minute = datetime.now().minute
+
+    if minute >= 45:
+        h = hour + 1
+    else:
+        h = hour
 
     end_results = {}
-    for i in range(hour):
+    for i in range(h):
         if i < 10:
             base_time = "0" + str(i) + "00"
         else:
@@ -113,8 +118,7 @@ def UltraSrtNcst(request):
                     "ny=" + ny
                     
         res = requests.get(weather_url + payload)
-        res_data = res.json()
-        items = res_data.get('response').get('body').get('items').get('item')
+        items = res.json().get('response').get('body').get('items').get('item')
 
 
         pty_dict = {
@@ -131,7 +135,7 @@ def UltraSrtNcst(request):
         for item_dict in items:
             if item_dict.get('category') == "T1H":
                 result["temperature"] = item_dict.get('obsrValue') + "°C"
-            if item_dict.get('category') == "R1N":
+            if item_dict.get('category') == "RN1":
                 result["precipitation"] = item_dict.get('obsrValue') +"mm"
             if item_dict.get('category') == "REH":
                 result["humidity"] = item_dict.get('obsrValue') + "%"
@@ -141,26 +145,26 @@ def UltraSrtNcst(request):
 
         end_results[base_time] = result
 
-    # return Response(end_results)
+    return Response(end_results)
 
-    res = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": "weather for now: " + end_results["0000"]["temperature"]
-                    }
-                }
-            ]
-        }
-    }
+    # res = {
+    #     "version": "2.0",
+    #     "template": {
+    #         "outputs": [
+    #             {
+    #                 "simpleText": {
+    #                     "text": "weather for now: " + end_results["0000"]["temperature"]
+    #                 }
+    #             }
+    #         ]
+    #     }
+    # }
 
-    return JsonResponse(res)
+    # return JsonResponse(res)
 
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 def UltraSrtFcst(request):
 
     weather_url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?"
@@ -168,7 +172,9 @@ def UltraSrtFcst(request):
     service_key = "1gBJqXUPjw9pRznJDfyVledJCQop%2B%2BUzpFZhWjhGrMZKjQ305PGGxoTr%2BGSNYpMExVLCQRIsBtI2ccZmKoxK%2Bg%3D%3D"
 
     today = date.today()
+    # yesterday = datetime.today() - timedelta(days=1)
     base_date = today.strftime("%Y%m%d")
+    # base_date = yesterday.strftime("%Y%m%d")
 
     hour = datetime.now().hour
     minute = datetime.now().minute
@@ -177,7 +183,7 @@ def UltraSrtFcst(request):
         h = hour
     else:
         h = hour - 1
-        
+
     if h < 10:
         base_time = "0" + str(h) + "30"
     else:
@@ -196,18 +202,18 @@ def UltraSrtFcst(request):
                 "numOfRows=60"
 
     res = requests.get(weather_url + payload)
-    return Response(res.json())
+    # return Response(res.json())
 
     items_dict_list = res.json().get('response').get('body').get('items').get('item')
 
-    time_list = []
-    for i in range(7):
-        fcst_h = h + i
-        if fcst_h < 10:
-            fcst_time = "0" + str(fcst_h) + "00"
-        else:
-            fcst_time = str(fcst_h) + "00"
-    time_list.append(fcst_time)
+    # time_list = []
+    # for i in range(7):
+    #     fcst_h = h + i
+    #     if fcst_h < 10:
+    #         fcst_time = "0" + str(fcst_h) + "00"
+    #     else:
+    #         fcst_time = str(fcst_h) + "00"
+    # time_list.append(fcst_time)
 
     pty_dict = {
         "0": "없음",
@@ -228,41 +234,30 @@ def UltraSrtFcst(request):
 
     result = {}
 
-    for time in time_list:
-        result["time"] = []
+    for item_dict in items_dict_list:
+        time = item_dict.get('fcstTime')
+        result[time] = dict()
 
     for item_dict in items_dict_list:
+        time = item_dict.get('fcstTime')
         if item_dict.get('category') == "T1H":
-            sub_result = dict()
-            sub_result["temperature"] = item_dict.get('obsrValue') + "°C"
-            time = item_dict.get('fcstTime')
-            result[time].append(sub_result)
+            result[time]["temperature"] = item_dict.get('fcstValue') + "°C"
 
-        if item_dict.get('category') == "RN1":
-            sub_result = dict()
-            sub_result["precipitation"] = item_dict.get('obsrValue')
-            time = item_dict.get('fcstTime')
-            result[time].append(sub_result)
+        elif item_dict.get('category') == "RN1":
+            result[time]["precipitation"] = item_dict.get('fcstValue')
 
-        if item_dict.get('category') == "SKY":
-            sub_result = dict()
-            obsr_value = item_dict.get('obsrValue')
-            sub_result["atomosphere"] = sky_dict.get(obsr_value)
-            time = item_dict.get('fcstTime')
-            result[time].append(sub_result)
+        elif item_dict.get('category') == "SKY":
+            obsr_value = item_dict.get('fcstValue')
+            result[time]["atomosphere"] = sky_dict.get(obsr_value)
 
-        if item_dict.get('category') == "PTY":
-            sub_result = dict()
-            obsr_value = item_dict.get('obsrValue')
-            sub_result["rain_type"] = pty_dict.get(obsr_value)
-            time = item_dict.get('fcstTime')
-            result[time].append(sub_result)
+        elif item_dict.get('category') == "PTY":
+            obsr_value = item_dict.get('fcstValue')
+            result[time]["rain_type"] = pty_dict.get(obsr_value)
 
     return Response(result)
 
 
-
-@api_view(["POST"])
+@api_view(["GET"])
 def VilageFcst(request):
     
     weather_url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?"
@@ -272,7 +267,7 @@ def VilageFcst(request):
     today = date.today()
     base_date = today.strftime("%Y%m%d")
 
-    base_time = "0700"
+    base_time = "0500"
 
     nx = "61"
     ny = "126"
@@ -282,10 +277,61 @@ def VilageFcst(request):
                 "base_date=" + base_date + "&" +\
                 "base_time=" + base_time + "&" +\
                 "nx=" + nx + "&" +\
-                "ny=" + ny
+                "ny=" + ny + "&" +\
+                "numOfRows=809"
                 
     res = requests.get(weather_url + payload)
-    
-    print(res)
-    return Response(res.json())
-    #items = res.json().get('reponse').get('body').get('items')
+    # return Response(res.json())
+
+    items_dict_list = res.json().get('response').get('body').get('items').get('item')
+
+    pty_dict = {
+        "0": "없음",
+        "1": "비",
+        "2": "비/눈",
+        "3": "눈",
+        "4": "소나기",
+    }
+
+    sky_dict = {
+        "1": "맑음",
+        "2": "구름조금",
+        "3": "구름많음",
+        "4": "흐림",
+    }
+
+    result = {}
+    result["baseDate"] = base_date
+    result["baseTime"] = base_time
+    # result["fcstTime"] = []
+
+    for item_dict in items_dict_list:
+        time = item_dict.get('fcstTime')
+        result[time] = dict()
+
+    for item_dict in items_dict_list:
+        time = item_dict.get('fcstTime')
+        if item_dict.get('category') == "POP":
+            result[time]["rain_prob"] = item_dict.get('fcstValue') + "%"
+
+        elif item_dict.get('category') == "PTY":
+            obsr_value = item_dict.get('fcstValue')
+            result[time]["rain_type"] = pty_dict.get(obsr_value)
+
+        elif item_dict.get('category') == "PCP":
+            result[time]["1h_precipitation"] = item_dict.get('fcstValue')
+
+        elif item_dict.get('category') == "SKY":
+            obsr_value = item_dict.get('fcstValue')
+            result[time]["atomosphere"] = sky_dict.get(obsr_value)
+
+        elif item_dict.get('category') == "TMP":
+            result[time]["1h_temp"] = item_dict.get('fcstValue') + "°C"
+
+        elif item_dict.get('category') == "TMX":
+            result[time]["highest_temp"] = item_dict.get('fcstValue') + "°C"
+
+        elif item_dict.get('category') == "TMN":
+            result[time]["lowest_temp"] = item_dict.get('fcstValue') + "°C"
+
+    return Response(result)
